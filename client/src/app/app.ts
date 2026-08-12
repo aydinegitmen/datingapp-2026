0ccdee032645c1c724f0e49f63546d48350d5f95
Aydın Eğitmen <aydinegitmen2001@gmail.com>
2026-08-12 16:26:51 +0300

    end of section 3

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected  title = 'client';
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected  title = 'Dating App';
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+  this.members.set(await this.getMembers())
 }
+async getMembers() {
+  try {
+  return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+} catch (error) {
+  console.log(error);
+  throw error;  
+  }
+}
+}
+
