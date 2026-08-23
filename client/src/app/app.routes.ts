fe5ba6f563e069436780173293bd9f2bad9ca712
Aydın Eğitmen <aydinegitmen2001@gmail.com>
2026-08-23 12:52:49 +0300

    end of 16

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -13,33 +13,40 @@ import { MemberPhotos } from '../features/members/member-photos/member-photos';
 import { MemberMessages } from '../features/members/member-messages/member-messages';
 import { memberResolver } from '../features/members/member-resolver';
 import { preventUnsavedChangesGuard } from '../core/guards/prevent-unsaved-changes-guard';
+import { Admin } from '../features/admin/admin';
+import { adminGuard } from '../core/guards/admin-guard';
 
 export const routes: Routes = [
-    { path: '', component: Home },
-    {
-        path: '',
+  { path: '', component: Home },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'members', component: MemberList },
+      {
+        path: 'members/:id',
+        resolve: { member: memberResolver },
         runGuardsAndResolvers: 'always',
-        canActivate: [authGuard],
+        component: MemberDetailed,
         children: [
-            { path: 'members', component: MemberList },
-            {
-                path: 'members/:id',
-                resolve: { member: memberResolver },
-                runGuardsAndResolvers:'always',
-                component: MemberDetailed,
-                children: [
-                    { path: '', redirectTo: 'profile', pathMatch: 'full' },
-                    { path: 'profile', component: MemberProfile, title: 'Profile' , 
-                        canDeactivate: [preventUnsavedChangesGuard]},
-                    { path: 'photos', component: MemberPhotos, title: 'Photos' },
-                    { path: 'messages', component: MemberMessages, title: 'Messages' }
-                ]
-            },
-            { path: 'lists', component: Lists },
-            { path: 'messages', component: Messages },]
-
-    },
-    { path: 'errors', component: TestErrors },
-    { path: 'server-error', component: ServerError },
-    { path: '**', component: NotFound },
+          { path: '', redirectTo: 'profile', pathMatch: 'full' },
+          {
+            path: 'profile',
+            component: MemberProfile,
+            title: 'Profile',
+            canDeactivate: [preventUnsavedChangesGuard],
+          },
+          { path: 'photos', component: MemberPhotos, title: 'Photos' },
+          { path: 'messages', component: MemberMessages, title: 'Messages' },
+        ],
+      },
+      { path: 'lists', component: Lists },
+      { path: 'messages', component: Messages },
+      { path: 'admin', component: Admin, canActivate:[adminGuard] },
+    ],
+  },
+  { path: 'errors', component: TestErrors },
+  { path: 'server-error', component: ServerError },
+  { path: '**', component: NotFound },
 ];
